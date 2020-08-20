66304ea4b33b1df0a353ffb2803c14fde0b68f91
Usama <ussama.ahmed05@gmail.com>
2020-08-20 18:19:00 +0500

    Add project files.

---
+++ BasicApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicApplication.MvcApplication" Language="C#" %>
---
+++ BasicApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicApplication.Startup))]
+namespace BasicApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
